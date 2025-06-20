970fd476d89feb67005766a317ed122ebded2cba
Nikola Todorovic <nikolatodorovic92@gmail.com>
2025-06-20 17:20:52 +0200

    Fix

--- Billups.Application/Interfaces/IGameHistoryService.cs
+++ Billups.Application/Interfaces/IGameHistoryService.cs
@@ -6,4 +6,5 @@ public interface IGameHistoryService
 {
     public Task<IEnumerable<GameHistoryDto>> GetRecentHistoryAsync(CancellationToken cancellationToken);
     Task SaveAsync(GameHistoryDto history, CancellationToken cancellationToken);
+    Task ResetHistory(CancellationToken cancellationToken);
 }
